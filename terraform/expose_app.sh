6d9d2721a3057351d2167f86000e66e9e972b90d
gaith mtiri <gaithmtiri@gmail.com>
2022-12-19 07:47:31 -0500

    dds

---
+++ terraform/expose_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash         
+            
+cd istio-1.16.1
+export PATH=$PWD/bin:$PATH
+sudo kubectl apply -f samples/bookinfo/networking/bookinfo-gateway.yaml
+export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+export INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}')
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+echo "$GATEWAY_URL"
